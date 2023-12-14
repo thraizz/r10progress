8a23887070db7f456dc5c64576688e44532c2f91
Aron Schüler <aron.schueler@ambient.digital>
2023-12-14 17:36:00 +0100

    feat: hide usermenu content when not authenticated

--- src/UserMenu.tsx
+++ src/UserMenu.tsx
@@ -1,16 +1,24 @@
+import { useContext } from "react";
 import { UploadModal } from "./UploadModal";
+import { UserContext } from "./UserProvider";
 import { auth } from "./firebaseConfig";
 
 export const UserMenu = () => {
+  const { user } = useContext(UserContext);
+  const isAuthenticated = user?.uid;
   return (
     <div className="flex flex-row gap-4 items-center">
-      <UploadModal />
-      <button
-        onClick={() => auth.signOut()}
-        className="border-2 border-gray-300 px-4 py-2 rounded-md bg-sky-200"
-      >
-        Logout
-      </button>
+      {isAuthenticated && (
+        <>
+          <UploadModal />
+          <button
+            onClick={() => auth.signOut()}
+            className="border-2 border-gray-300 px-4 py-2 rounded-md bg-sky-200"
+          >
+            Logout
+          </button>
+        </>
+      )}
     </div>
   );
 };
