e53f712e175483cb872b11710e88abe26a72f0be
Aron Schüler <aron.schueler@ambient.digital>
2024-03-02 20:05:01 +0100

    style: adjust listbox width on mobile

--- src/components/base/BaseListbox.tsx
+++ src/components/base/BaseListbox.tsx
@@ -20,7 +20,7 @@ export const BaseListbox = ({
         setOption(value);
       }}
     >
-      <div className="relative mt-1 min-w-[15rem]">
+      <div className="relative mt-1 w-full lg:min-w-[15rem]">
         <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
           <span className="block truncate">{valueText}</span>
           <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
