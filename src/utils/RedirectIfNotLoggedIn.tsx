8c72a4c24d2d66795e39236ca2ba6a5ba9fb60f9
Aron Schüler <aron.schueler@ambient.digital>
2024-03-02 23:32:29 +0100

    fix: add missing component for redirect

--- src/utils/RedirectIfNotLoggedIn.tsx
+++ src/utils/RedirectIfNotLoggedIn.tsx
@@ -2,8 +2,14 @@ import { PropsWithChildren, useContext } from "react";
 import { Navigate } from "react-router-dom";
 import { UserContext } from "../provider/UserContext";
 
-export const RedirectIfSignedIn = ({ children }: PropsWithChildren) => {
+export const RedirectIfNotLoggedIn = ({ children }: PropsWithChildren) => {
   const { user } = useContext(UserContext);
 
   return !user?.uid ? <Navigate to="/login" replace /> : children;
+};
+
+export const RedirectIfSignedIn = ({ children }: PropsWithChildren) => {
+  const { user } = useContext(UserContext);
+
+  return user?.uid ? <Navigate to="/dashboard" replace /> : children;
 };
