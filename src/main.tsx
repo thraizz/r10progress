7705297d32a5fad556c5b2d8149ee2340795f200
Aron Schüler <aron.schueler@ambient.digital>
2024-06-11 12:11:32 +0200

    feat: send non-main-site traffic to main site

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,11 @@ import { SettingsProvider } from "./provider/SettingsContext.tsx";
 import { UserProvider } from "./provider/UserContext.tsx";
 import { router } from "./router.tsx";
 
+// Redirect to main domain for non-development environments
+const MAIN_SITE = "app.r10progress.com";
+if (!import.meta.env.DEV && location.host !== MAIN_SITE) {
+  window.location.href = `https://${MAIN_SITE}${location.pathname}`;
+}
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <UserProvider>
