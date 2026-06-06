a3ae52bb814efe69cbed4f3c095cf43d2755c037
Aron Schüler <git@lakur.tech>
2026-06-06 19:06:17 +0200

    feat(nav): re-add upload entry to top bar as link to upload page

--- src/components/UserMenu.tsx
+++ src/components/UserMenu.tsx
@@ -1,6 +1,8 @@
 import { useContext } from "react";
+import { Link } from "react-router-dom";
 import { auth } from "../firebase";
 import { UserContext } from "../provider/UserContext";
+import { dashboardRoutes } from "../routes";
 
 export const UserMenu = () => {
   const { user } = useContext(UserContext);
@@ -8,15 +10,23 @@ export const UserMenu = () => {
   return (
     <div className="flex flex-row items-center gap-4">
       {isAuthenticated && (
-        <button
-          onClick={() => {
-            auth.signOut();
-            window.location.assign("/");
-          }}
-          className="btn"
-        >
-          Logout
-        </button>
+        <>
+          <Link
+            to={dashboardRoutes.upload}
+            className="btn btn-secondary whitespace-nowrap"
+          >
+            Upload File
+          </Link>
+          <button
+            onClick={() => {
+              auth.signOut();
+              window.location.assign("/");
+            }}
+            className="btn"
+          >
+            Logout
+          </button>
+        </>
       )}
     </div>
   );
